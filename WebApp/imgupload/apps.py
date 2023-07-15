862de399c84cc85094634e43dbd7281b561039cd
Aakash R <84120773+Aakash67@users.noreply.github.com>
2023-07-15 23:31:12 +0530

    Delete WebApp directory

--- WebApp/imgupload/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImguploadConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'imgupload'
--- WebApp/imgupload/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from django.contrib import admin
-from . import views
-urlpatterns = [
-    re_path(r'^$',views.home, name='home.html'),
-]
